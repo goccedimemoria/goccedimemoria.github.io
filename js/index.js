8b75f7c954b72e3746da9391ee38175fa12c4dce
Luca Bonfante <bonfanteluca98@gmail.com>
2017-02-06 18:38:50 +0100

    aggiunto conclusioni e musica

--- js/index.js
+++ js/index.js
@@ -1,52 +1,84 @@
 app = {
-    
-    inizializza: function() {
+    inizializza: function () {
         this.arg01();
         this.arg02();
         this.arg03();
         this.arg04();
         this.arg05();
         this.arg06();
+        this.prova();
     },
-    
-    arg01: function() {
-        $('#arg01').on('click', function() {
+    arg01: function () {
+        $('#arg01').on('click', function () {
             window.location = '/antisemitismo_nella_storia/';
         });
     },
-    
-    arg02: function() {
-        $('#arg02').on('click', function() {
+    arg02: function () {
+        $('#arg02').on('click', function () {
             window.location = '/legislazione_antisemita_germania_italia/';
         });
     },
-    
-    arg03: function() {
-        $('#arg03').on('click', function() {
+    arg03: function () {
+        $('#arg03').on('click', function () {
             window.location = '/geografia_dello_sterminio/';
         });
     },
-    
-    arg04: function() {
-        $('#arg04').on('click', function() {
+    arg04: function () {
+        $('#arg04').on('click', function () {
             window.location = '/vita_nei_lager/';
         });
     },
-    
-    arg05: function() {
-        $('#arg05').on('click', function() {
+    arg05: function () {
+        $('#arg05').on('click', function () {
             window.location = '/testimonianze/';
         });
     },
-    
-    arg06: function() {
-        $('#arg06').on('click', function() {
+    arg06: function () {
+        $('#arg06').on('click', function () {
             window.location = '/ripensare_la_shoah/';
         });
+    },
+    prova: function () {
+        function Utils() {
+
+        }
+
+        Utils.prototype = {
+            constructor: Utils,
+            isElementInView: function (element, fullyInView) {
+                var pageTop = $(window).scrollTop();
+                var pageBottom = pageTop + $(window).height();
+                var elementTop = $(element).offset().top;
+                var elementBottom = elementTop + $(element).height();
+
+                if (fullyInView === true) {
+                    return ((pageTop < elementTop) && (pageBottom > elementBottom));
+                } else {
+                    return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
+                }
+            }
+        };
+
+        var Utils = new Utils();
+
+        var isElementInView = Utils.isElementInView($('#div-audio'), false);
+
+        var audio = document.getElementById("audio");
+
+        if (isElementInView) {
+            //$('#audio').play();
+            audio.play();
+        } else {
+            audio.pause();
+        }
     }
-    
+
+
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
     app.inizializza();
+
 });
+
+
